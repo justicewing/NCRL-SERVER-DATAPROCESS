959aed3b47efb25d72993cbc9f96917498f72894
SherlockHsu <SherlockHsu@foxmail.com>
2018-05-08 21:21:33 +0800

    update plot

--- matlab/plotDetect.m
+++ matlab/plotDetect.m
@@ -1,8 +1,8 @@
 close all;
 clear;
-BER_ideal = importdata('statistic\BER_ideal.txt','%s');
-BER_ideal_pilot = importdata('statistic\BER_ideal_pilot.txt','%s');
-BER_detect = importdata('statistic\BER_detect.txt','%s');
+BER_ideal = importdata('statistics\BER_ideal.txt','%s');
+BER_ideal_pilot = importdata('statistics\BER_ideal_pilot.txt','%s');
+BER_detect = importdata('statistics\BER_detect.txt','%s');
 SNR = 0:50;
 
 lineWidth=1.5;
@@ -23,18 +23,3 @@ for i = 1:15
 	grid on;
 
 end
-
-figure;
-lineWidth=1.5;
-semilogy(SNR,BER_ideal,'LineWidth',lineWidth);
-hold on;
-semilogy(SNR,BER_ideal_pilot,'LineWidth',lineWidth);
-semilogy(SNR,BER_detect,'LineWidth',lineWidth);
-legend('理想完整信道',...
-	'理想导频信道',...
-	'估计信道'...
-	);
-xlabel('Eb/N0(dB)');
-ylabel('BER');
-title('选择不同估计信道的误码曲线(numFlow=8,CQI=15)');
-grid on;
